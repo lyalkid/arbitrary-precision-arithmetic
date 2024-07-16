9f0bf6421fdd53dba76ffcbc6d82af4a996d9a30
Azamat Gaifullin s21:sionapae <agaifullin157@gmail.com>
2024-07-17 00:11:37 +0300

    update todo

---
+++ python/main.py
@@ -0,0 +1,8 @@
+a = 156278
+b = 32188
+
+print(f'a - b = {a - b}' )
+print(f'a + (~b) + 1 = {a + (~b) + 1}')
+print("\n")
+print(f'a : {a}')
+print(f'~b : {~b}')
--- s21_add.c
+++ s21_add.c
@@ -59,6 +59,29 @@ void normalize_scale(int* scale1, int* scale2) {
     }
 }
 
+
+/*
+    скалдывает два числа, которые представлены в виде массива
+*/
+void get_add(const int value1[], const int value2[], int coef[], int size, int base) {
+    for (int i = 0; i < size; i++) {
+            coef[i] = value1[i] + value2[i];
+    }
+    add_normalize(coef, size, base);
+}
+
+/* 
+    функция получает набор (массив) коэффициентов и нормализует их в соотвествии с выбранной 
+    системой счисления. Например, сложили два числа 52 + 48, получили 100.
+    Как это выглядит в виде массива: 
+    [2, 5, 0]
+    +
+    [8, 4, 0]
+    =
+    [10, 9]
+    после нормализации:
+    [0, 0, 1]
+*/
 void add_normalize(int coef[], int size, int base) {
     int carry = 0;      // сколько данных перешло в след разряд
     int remainder = 0;  // остаток
@@ -75,18 +98,6 @@ void add_normalize(int coef[], int size, int base) {
         carry /= base;
     }
 }
-
-void get_add(const int value1[], const int value2[], int coef[], int size, int base) {
-    for (int i = 0; i < size; i++) {
-        if (i >= size) {
-            coef[i] = 0;
-        } else {
-            coef[i] = value1[i] + value2[i];
-        }
-    }
-    add_normalize(coef, size, base);
-}
-
 // size = DEC_ARRAY
 void from_decimal_to_array(s21_decimal decimal, int array[]) {
     init_array(array, DEC_ARRAY);
