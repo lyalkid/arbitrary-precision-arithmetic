0fee11db6b6efcbe85e2e652ff320e989a78eb91
Azamat Gaifullin s21:sionapae <agaifullin157@gmail.com>
2024-07-26 22:09:26 +0300

    depimal

--- arithmetic_helpers.c
+++ arithmetic_helpers.c
@@ -39,3 +39,66 @@ void add_normalize(int coef[], int size, int base) {
     }
 }
 
+// умножение большого числа на маленькое
+// массив, число, результат, размер массива, основание системы
+// -------------------------------------------
+void simple_multiply(int a[], int b, int result[], int size_a,
+                     int base) {  // size - размер числа a  , b - число : example a = 05678 , b = 3,
+    // result = 00000, size = 5, base -  основание системы счисления
+    init_array(result, size_a);  // заполняем нулями массив result
+    int carry = 0;  // нужно для хранения количества перехода в другой разряд
+    int i = 0;  // индекс
+    while (i < size_a) {
+        int mult = a[i] * b + carry;
+        int remainder = mult % base;  // остаток
+        result[i] = remainder;
+        carry = mult / base;
+        i++;
+    }
+}
+// -------------------------------------------
+
+int get_real_len_of_number(int a[], int size) {
+    int i = 0;
+    int len = 0;
+    int count = 0;
+    for (i = size - 1; i >= 0; i--) {
+        if (a[i] == 0) {
+            count++;
+        } else {
+            break;
+        }
+    }
+
+    return size - count;
+}
+
+// умножение большого числа на большое
+// -------------------------------------------
+void multiply(int a[], int b[], int result[], int size) {
+    int base = 10;
+    // size = 128 все три массива размера size
+    int simple_mult[size];  // массив для промежуточных произведений
+    init_array(simple_mult, size);
+    int n = size, m = size;
+
+    int current_index = 0;
+
+    for (int i = 0; i < m; i++) {
+        simple_multiply(a, b[i], simple_mult, size, base);
+        current_index = i;
+        int k = 0;
+        int end_current_index = current_index + get_real_len_of_number(simple_mult, size);
+        for (int j = current_index; j < end_current_index; j++) {
+            result[j] += simple_mult[k];
+            k++;
+        }
+        add_normalize(result, size, base);
+    }
+}
+
+void init_array(int a[], int n) {
+    for (int i = 0; i < n; i++) {
+        a[i] = 0;
+    }
+}
--- arithmetic_helpers.h
+++ arithmetic_helpers.h
@@ -5,7 +5,9 @@
 
 void get_add(const int value1[], const int value2[], int coef[], int size);
 void add_normalize(int coef[], int size, int base) ;
-
-
-
+void init_array(int a[], int n);
+void simple_multiply(int a[], int b, int result[], int size_a,
+                     int base);
+int get_real_len_of_number(int a[], int size);
+void multiply(int a[], int b[], int result[], int size);
 #endif // ARITHMETIC_HELPERS_H_
