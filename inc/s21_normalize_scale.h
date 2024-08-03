24c9e14e8af13d59a153aebdf5e26b5e86f9314f
Azamat Gaifullin <agaifullin157@gmail.com>
2024-08-03 11:41:58 +0300

    almoust addition for s21_decimal

--- inc/s21_normalize_scale.h
+++
@@ -1,9 +0,0 @@
-#ifndef S21_NORMALIZE_SCALE_H_
-#define S21_NORMALIZE_SCALE_H_
-#include "s21_decimal.h"
-
-void normalization(s21_decimal decimal_1, s21_decimal decimal_2);
-void normalize_arrays(int value1[], int value2[], int size, int* scale1, int* scale2);
-int normalize_scale(int* scale1, int* scale2);
-
-#endif  // S21_NORMALIZE_SCALE_H_
